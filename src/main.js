4a2ab398ada706258b1e165ec01be28799ed6236
mishabiletskyi <140491531+mishabiletskyi@users.noreply.github.com>
2025-08-26 18:03:06 +0300

    feat: add Intercom integration

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { initIntercom } from './intercom'
 
 const app = createApp(App)
 
 app.use(router)
+
+initIntercom()
 
 app.mount('#app')
